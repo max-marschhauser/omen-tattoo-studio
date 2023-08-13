5b28e04aa8e48aedfff280e0070e476480c7ef50
max-marschhauser <max.marschhauser@gmail.com>
2023-08-13 19:24:43 +0200

    navbar border fix

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -4,13 +4,9 @@ import "./home.scss";
 export default function Home() {
 	return (
 		<>
-			<div className="box">
-				<div className="box-inner">
-					<div className="full-screen">
-						<NavbarHome />
-						<p className="homePageQuote">"TURN AND FACE THE STRANGE."</p>
-					</div>
-				</div>
+			<div className="full-screen">
+				<NavbarHome />
+				<p className="homePageQuote">"TURN AND FACE THE STRANGE."</p>
 			</div>
 		</>
 	);
