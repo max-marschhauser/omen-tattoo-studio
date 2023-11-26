0b445d71cf74634bfbd77f02943b8bcfa9eee26e
max-marschhauser <max.marschhauser@gmail.com>
2023-11-26 15:05:49 +0100

    savjeti icons fix

--- src/pages/savjeti/components/expandable.tsx
+++ src/pages/savjeti/components/expandable.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-
 import "../savjeti.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 interface ExpandableProps {
 	id: number;
@@ -12,16 +13,16 @@ const Expandable: React.FC<ExpandableProps> = ({ id, title, subtext }) => {
 	const [expanded, setExpanded] = useState(false);
 
 	function toggleExpanded() {
-		setExpanded(!expanded);
+		setExpanded((prev) => !prev);
 	}
 
 	return (
-		<div className="expandable-item" onClick={toggleExpanded}>
-			<button>
+		<div className="expandable-item">
+			<button onClick={toggleExpanded}>
 				<h2>
 					{id + 1}. {title}
 				</h2>
-				<i className="fa-solid fa-arrow-down"></i>
+				{expanded ? <FontAwesomeIcon icon={faArrowUp} /> : <FontAwesomeIcon icon={faArrowDown} />}
 			</button>
 			{expanded && <p>{subtext}</p>}
 		</div>
